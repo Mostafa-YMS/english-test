287d4b2c31d7fdd9ce967340b22d1552765a9c72
Mostafa-YMS <mos_302@yahoo.com>
2022-10-29 01:16:40 +0200

    use cors

--- server/index.ts
+++ server/index.ts
@@ -2,11 +2,14 @@ import express, { Express } from "express";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import { serverRouter } from "./routes/index";
+import cors from "cors";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
+
+app.use(cors());
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
