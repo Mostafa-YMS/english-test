3b0711afc2b032fe1c24b07ac09fb7e0e33d8682
Mostafa-YMS <mos_302@yahoo.com>
2022-10-29 12:51:30 +0200

    answer question

--- client/src/pages/practice/index.jsx
+++ client/src/pages/practice/index.jsx
@@ -2,9 +2,14 @@ import { useCallback } from "react";
 import { useState } from "react";
 import { useEffect } from "react";
 import { getWordsService } from "../../services";
+import "../../styles/practice.css";
+
+const wordPos = ["verb", "adverb", "noun", "adjective"];
 
 const Practice = () => {
   const [words, setWords] = useState();
+  const [score, setScore] = useState(0);
+  const [rightAnswer, setRightAnswer] = useState();
   const [loading, setLoading] = useState(false);
   const [currentWord, setCurrentWord] = useState(0);
 
@@ -23,7 +28,65 @@ const Practice = () => {
     getWords && getWords();
   }, [getWords]);
 
-  return <div>Practice</div>;
+  const answerQuestion = ({ target }) => {
+    if (target?.name === words[currentWord]?.pos) {
+      setRightAnswer(true);
+      setScore((prev) => prev + 1);
+      target?.classList.add("btn-right");
+    } else {
+      setRightAnswer(false);
+      target?.classList.add("btn-wrong");
+    }
+  };
+
+  return (
+    <>
+      {words && !loading && (
+        <div className="paper">
+          <h2>Practice</h2>
+
+          <p className="word">{words[currentWord]?.word}</p>
+
+          <p
+            className="txt-right"
+            style={{
+              visibility: rightAnswer === true ? "visible" : "hidden",
+            }}
+          >
+            Right answer
+          </p>
+          <p
+            className="txt-wrong"
+            style={{
+              visibility: rightAnswer === false ? "visible" : "hidden",
+            }}
+          >
+            Wrong answer
+          </p>
+
+          <p>Choose this word part of speech:</p>
+          <div className="btns-div">
+            {wordPos?.map((e, i) => (
+              <button
+                key={i}
+                onClick={answerQuestion}
+                name={e}
+                disabled={typeof rightAnswer === "boolean"}
+              >
+                {e}
+              </button>
+            ))}
+          </div>
+
+          <div className="div-next">
+            {typeof rightAnswer === "boolean" && <button>Next Question</button>}
+          </div>
+        </div>
+      )}
+
+      {loading && <p>Loading ...</p>}
+    </>
+  );
 };
 
 export default Practice;
